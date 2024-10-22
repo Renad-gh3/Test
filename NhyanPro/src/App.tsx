f1ccb1bc2679f02d9901deed9e59c9009c76f701
Renad Alghamdi <Renad.alghamdi.13@gmail.com>
2024-10-22 23:05:57 +0300

    test

---
+++ NhyanPro/src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section
+      dir="rtl"
+      className="grid h-svh w-svh grid-rows-[1fr_1fr] grid-cols-[1fr_1fr]  "
+    >
+      <div className="bg-red-500 ">Nhyan</div>
+      <div className=" bg-lime-500 col-start-2  row-start-1 row-end-[-1]">
+        Renad
+      </div>
+      <div className=" bg-sky-500">Rama</div>
+    </section>
+  );
+}
+
+export default App;
